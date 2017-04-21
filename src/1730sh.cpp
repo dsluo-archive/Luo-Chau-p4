49ddb1eb95ae3d59aee1d67b6845abac240ed2c6
benschau <bchau3440@gmail.com>
2017-04-20 20:58:57 -0400

    working cwd prompt

--- src/1730sh.cpp
+++ src/1730sh.cpp
@@ -3,46 +3,20 @@
 #include <iostream>
 #include <vector>
 #include <iterator>
+#include <unistd.h>
+#include <string.h>
 
 using namespace std;
 
-vector<string> tokenize(string str) {
-    vector<string> output;
-    
-    char prev = '\0';
-
-    for (auto it = str.begin(); it != str.end(); it++) {
-        if (*it == ' ' && prev != '\0' && prev != ' ') {
-            output.push_back({""});
-        } else if (*it == '"') {
-            while (++it != str.end()) {
-                if (*it == '\\' && *(it + 1) == '"') {
-                    output.back().push_back('"');
-                    it++;
-                } else if (*it == '"') {
-                    break;
-                } else {
-                    output.back().push_back(*it);
-                }
-                //it++;
-            }
-                 
-        } else if (*it != ' ') {
-            if (output.empty())
-                output.push_back("");
-            output.back().push_back(*it);
-        }
-        prev = *it;
-    }
-
-    return output;
-}
+vector<string> tokenize(string str);
+void printprompt();
 
 int main() {
 
     string line;
-    printf("repl$ ");
-    while (getline(cin, line)) {
+    while (true) {
+        printprompt();
+        getline(cin, line);
 
         int pipe_count = 0;
 
@@ -139,4 +113,60 @@ int main() {
     }
     printf("\n");
     return EXIT_SUCCESS;
+}
+
+void printprompt(){
+    // figure out what the current working directory is. 
+    long maxlen = pathconf(".", _PC_PATH_MAX);
+    char *buf = nullptr;
+    char *p; 
+    if ((p = getcwd(buf, maxlen)) == NULL)
+        perror("getcwd");
+    
+    // If the user is above their home directory, use a tilde.
+    // If not, print the absolute path instead.
+    const char *homedir = getenv("HOME");
+    if (homedir == NULL)
+        perror("getenv");     
+   
+    // Check the current working directory versus the home directory.
+    printf("1730sh: ");
+    if (strlen(p) > strlen(homedir)){
+        char *rpath = p + strlen(homedir);
+        printf("~%s> ", rpath);   
+    } else {
+        printf("%s> ", p); 
+    }
+}
+
+vector<string> tokenize(string str) {
+    vector<string> output;
+    
+    char prev = '\0';
+
+    for (auto it = str.begin(); it != str.end(); it++) {
+        if (*it == ' ' && prev != '\0' && prev != ' ') {
+            output.push_back({""});
+        } else if (*it == '"') {
+            while (++it != str.end()) {
+                if (*it == '\\' && *(it + 1) == '"') {
+                    output.back().push_back('"');
+                    it++;
+                } else if (*it == '"') {
+                    break;
+                } else {
+                    output.back().push_back(*it);
+                }
+                //it++;
+            }
+                 
+        } else if (*it != ' ') {
+            if (output.empty())
+                output.push_back("");
+            output.back().push_back(*it);
+        }
+        prev = *it;
+    }
+
+    return output;
 }
