bdbeb588111cd5cddad1918a059e16034817553f
davidsluo <davidsluo@users.noreply.github.com>
2017-04-25 21:50:22 -0400

    Added io redirection

--- src/1730sh.cpp
+++ src/1730sh.cpp
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <fcntl.h>
 
 #include "BuiltIns.h"
 
@@ -31,69 +32,53 @@ int main() {
         "                       If no signal is specified, the SIGTERM signal is sent.\n";
     string line;
     int * last_wstatus = nullptr;
-    
+
     signal(SIGINT, SIG_IGN);
-   
+
+    int dfl_in = dup(STDIN_FILENO);
+    if (dfl_in == -1) { perror("dup"); exit(EXIT_FAILURE); }
+    int dfl_out = dup(STDOUT_FILENO);
+    if (dfl_out == -1) { perror("dup"); exit(EXIT_FAILURE); }
+    int dfl_err = dup(STDERR_FILENO);
+    if (dfl_err == -1) { perror("dup"); exit(EXIT_FAILURE); }
+
     printprompt();
     while (getline(cin, line)) {
-        string in = "STDIN_FILENO";
-        string out = "STDOUT_FILENO";
-        string err = "STDERR_FILENO";
+        string in = "";
+        string out = "";
+        string err = "";
 
         int infd = STDIN_FILENO;
         int outfd = STDOUT_FILENO;
         int errfd = STDERR_FILENO;
 
+        bool out_append = false;
+        bool err_append = false;
+
         vector<string> tokens = tokenize(line);
-        vector<vector<string>> procs;
+        vector<vector<string>> procs; // TODO: Fix spacing splitting thing
         vector<string> proc;
-        
+
         for (auto it = tokens.begin(); it != tokens.end(); it++) {
             if ((it + 1) != tokens.end()) {
                 if (*it == "e>") { // TODO: Handling error redirection
+                    err_append = false;
                     err = *(it + 1);
-                     
-                    /* if ((errfd = open(err.c_str(), O_WRONLY)) == -1){ */
-                    /*     perror("open"); */
-                    /*     exit(EXIT_FAILURE); */ 
-                    /* } */ 
-                    
                     it++;
                 } else if (*it == "e>>") { // TODO: Handling error redirection
+                    err_append = true;
                     err = *(it + 1);
-
-                    /* if ((errfd = open(err.c_str(), O_WRONLY | O_APPEND)) == -1){ */
-                    /*     perror("open"); */
-                    /*     exit(EXIT_FAILURE); */ 
-                    /* } */ 
-
                     it++;
                 } else if (*it == ">") { // TODO: Handling output redirection
+                    out_append = false;
                     out = *(it + 1);
-                    
-                    /* if ((outfd = open(out.c_str(), O_WRONLY)) == -1){ */
-                    /*     perror("open"); */
-                    /*     exit(EXIT_FAILURE); */ 
-                    /* } */ 
-                    
                     it++;
                 } else if (*it == ">>") { // TODO: Handling output redirection
+                    out_append = true;
                     out = *(it + 1);
-                   
-                    /* if ((outfd = open(out.c_str(), O_WRONLY | O_APPEND)) == -1){ */
-                    /*     perror("open"); */
-                    /*     exit(EXIT_FAILURE); */ 
-                    /* } */ 
-                   
                     it++;
                 } else if (*it == "<") { // TODO: Handling input redirection
                     in = *(it + 1);
-                  
-                    /* if ((infd = open(in.c_str(), O_WRONLY)) == -1){ */
-                    /*     perror("open"); */
-                    /*     exit(EXIT_FAILURE); */ 
-                    /* } */ 
-                   
                     it++;
                 } else if (*it == "|") {
                     if (!proc.empty()) {
@@ -103,6 +88,8 @@ int main() {
                 } else {
                     proc.push_back(*it);
                 }
+                if (it == tokens.end() - 1)
+                    procs.push_back(proc);
             } else {
                 proc.push_back(*it);
                 procs.push_back(proc);
@@ -111,16 +98,9 @@ int main() {
 
         pid_t pid;
         vector<array<int, 2>> pipefds;
+
         // for each process in procs,
         for (size_t i = 0; i < procs.size(); i++) {
-            // Construct argv
-            char **argv = new char*[procs[i].size()];
-            for (size_t k = 0; k < procs[i].size(); k++) {
-                argv[k] = (char *) procs[i][k].c_str();
-            }
-            // argv must be null-terminated
-            argv[procs[i].size()] = nullptr;
-
             // create pipe for command
             if (i != procs.size() - 1){
                 int pipefd[2];
@@ -129,7 +109,15 @@ int main() {
                     exit(EXIT_FAILURE); 
                 }
                 pipefds.push_back({pipefd[0], pipefd[1]});
-            } 
+            }
+
+            // Construct argv
+            char **argv = new char*[procs[i].size()];
+            for (size_t k = 0; k < procs[i].size(); k++) {
+                argv[k] = (char *) procs[i][k].c_str();
+            }
+            // argv must be null-terminated
+            argv[procs[i].size()] = nullptr;
 
             if (strcmp(argv[0], "cd") == 0) {
                 cd(argv[1]);
@@ -156,68 +144,132 @@ int main() {
             } else if (strcmp(argv[0], "jobs") == 0) {  // TODO: Write jobs cmd
             } else if (strcmp(argv[0], "kill") == 0) {  // TODO: Write kill cmd
             } else {
-                // if the first command 
                 if (i == 0){
-                    // TODO: Handling stdin redirection 
+                    if (in != "") {
+                        // TODO: Handling stdin redirection 
+                        if ((infd = open(in.c_str(), O_RDONLY)) == -1){
+                            perror("open");
+                            exit(EXIT_FAILURE);
+                        }
+
+                        if (dup2(infd, STDIN_FILENO) == -1) {
+                            perror("dup2");
+                            exit(EXIT_FAILURE); }
+                    }
                 }
-                
-                // if the last command
+
+
                 if (i == procs.size() - 1){
-                    // TODO: Handle error redirection 
-                    // TODO: Handling stdout redirection
+                    if (out != "") {
+                        // TODO: Handling stdout redirection
+                        int mode = O_WRONLY | O_CREAT | (out_append ? O_APPEND : O_TRUNC);
+                        if ((outfd = open(out.c_str(), mode, 0644)) == -1){
+                            perror("open");
+                            exit(EXIT_FAILURE);
+                        }
+
+                        // if (dup2(outfd, STDOUT_FILENO) == -1) {
+                        //     perror("dup2");
+                        //     exit(EXIT_FAILURE);
+                        // }
+                    }
+                    if (err != "") {
+                        // TODO: Handle error redirection
+                        int mode = O_WRONLY | O_CREAT | (err_append ? O_APPEND : O_TRUNC);
+                        if ((errfd = open(err.c_str(), mode)) == -1){
+                            perror("open");
+                            exit(EXIT_FAILURE);
+                        }
+
+                        // if (dup2(errfd, STDERR_FILENO) == -1) {
+                        //     perror("dup2");
+                        //     exit(EXIT_FAILURE);
+                        // }
+                    }
                 }
-                
+
                 pid = fork();
-                
+
                 if (pid == 0){
                     // if not the first command 
-                    if (i != 0){
+                    if (i != 0){ 
                         if (dup2(pipefds.at(i - 1)[0], STDIN_FILENO) == -1){
                             perror("dup2");
                             exit(EXIT_FAILURE); 
                         }
+                    } else {
+                        if (dup2(errfd, STDERR_FILENO) == -1) {
+                            perror("dup2");
+                            exit(EXIT_FAILURE);
+                        }
                     }
-                    
-                    // if not the last command 
-                    if (i != procs.size() -1){
+                    // if not the last command
+                    if (i != procs.size() - 1){ 
                         if (dup2(pipefds.at(i)[1], STDOUT_FILENO) == -1){
                             perror("dup2");
                             exit(EXIT_FAILURE); 
                         }
+                    } else {
+                        if (dup2(outfd, STDOUT_FILENO) == -1){
+                            perror("dup2");
+                            exit(EXIT_FAILURE);
+                        }
                     }
+
 
                     for (unsigned int i = 0; i < pipefds.size(); i++){
                         close_pipe(pipefds.at(i).data()); 
                     }
 
                     // populate arguments with argv
-                    if (execvp(procs[i][0].c_str(), argv) == -1){
+                    int status = execvp(procs[i][0].c_str(), argv);
+                    delete[] argv;
+                    if (status == -1){
                         perror(procs[i][0].c_str()); 
-                        delete[] argv;
-                        
-                        exit(EXIT_FAILURE);
                     }
+
+                    if (status == -1)
+                        exit(EXIT_FAILURE);
+                    else
+                        exit(EXIT_SUCCESS);
+
                 } else if (pid < 0){
                     perror("fork"); 
                     exit(EXIT_FAILURE);
                 } 
+
             }
+
         }
-        
         // close all pipes when done
         for (unsigned int i = 0; i < pipefds.size(); i++){
             close_pipe(pipefds.at(i).data()); 
         }
-        
+
         waitpid(pid, last_wstatus, 0);
+
+        // Restore file descriptors 
+        if (dup2(STDIN_FILENO, dfl_in) == -1)
+            perror("dup2");
+        if (dup2(STDOUT_FILENO, dfl_out) == -1)
+            perror("dup2");
+        if (dup2(STDERR_FILENO, dfl_err) == -1)
+            perror("dup2");
+
+        if (infd != STDIN_FILENO && close(infd) != 0)
+            perror("close");
+        if (outfd != STDOUT_FILENO && close(outfd) != 0)
+            perror("close");
+        if (errfd != STDERR_FILENO && close(errfd) != 0)
+            perror("close");
 
         printprompt();
     }
-    
+
     printf("\n");
     return EXIT_SUCCESS;
 }
- 
+
 void printprompt(){
     // Figure out what the current working directory is. 
     long maxlen = pathconf(".", _PC_PATH_MAX);
@@ -225,13 +277,13 @@ void printprompt(){
     char *p; 
     if ((p = getcwd(buf, maxlen)) == NULL)
         perror("getcwd");
-    
+
     // If the user is above their home directory, use a tilde.
     // If not, print the absolute path instead.
     const char *homedir = getenv("HOME");
     if (homedir == NULL)
         perror("getenv");     
-   
+
     // Check the current working directory versus the home directory.
     printf("1730sh: ");
     if (strlen(p) >= strlen(homedir)){
@@ -244,9 +296,9 @@ void printprompt(){
 
 vector<string> tokenize(string str) {
     vector<string> output;
-    
+
     char prev = '\0';
-    
+
     for (auto it = str.begin(); it != str.end(); it++) {
         // Skip over excessive spaces
         if (*it == ' ' && prev != '\0' && prev != ' ') {
@@ -270,7 +322,7 @@ vector<string> tokenize(string str) {
                     output.back().push_back(*it);
                 }
             }
-        // Handle everything else
+            // Handle everything else
         } else if (*it != ' ') {
             if (output.empty())
                 output.push_back("");
